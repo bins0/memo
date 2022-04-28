fbfbeb7c9119552b170b72694cf0deac38edd98a
chaeeon <chaeeon37@naver.com>
2022-04-28 12:15:35 +0900

    first commit

---
+++ front/src/views/MemoList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>메모</h2>
+    <table>
+        <tr v-for="(memo,i) in memo" :key=i>
+            <td>{{memo.id}}</td>
+            <td>{{memo.writer}}</td>
+            <td @click="$router.push(`/list/${memo.id}`)">{{memo.title}}</td>
+        </tr>
+    </table>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return({
+            memo:[]
+        })
+    },
+    created() {
+        this.$http.get('/api/memo')
+        .then((response)=>{
+            this.memo = response.data;
+        })
+    }
+    
+}
+</script>
---
+++ front/src/views/WriteForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h3>글작성공간</h3>
+      
+            <input type="text" v-model="title"><br>
+            <textarea cols="30" rows="10" v-model="memo"></textarea><br>
+           <button @click="addmemo">전송</button>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            title: "",
+            memo: ""
+        }
+    },
+    methods : {
+        addmemo() {
+            this.$http.post('/api/memo',{
+                data : {
+                    title:this.title,
+                    memo : this.memo
+                }
+            }).then((response)=>console.log(response.data))
+            this.$router.push('/')
+        }
+    }
+}
+</script>
